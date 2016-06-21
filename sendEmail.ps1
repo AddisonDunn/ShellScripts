ef4f64bd70a28bf0f740082de10f1f91d1a0121b
AddisonDunn <DunnAddisonW@gmail.com>
2016-06-21 19:58:34 -0400

    sendEmail powershell

---
+++ sendEmail.ps1
@@ -0,0 +1,9 @@
+﻿$username = "DOMAIN_USERNAME"
+$password ="DOMAIN_PASSWORD"
+$secstr = New-Object -TypeName System.Security.SecureString
+$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $secstr
+$subject = "SUBJECT_LINE"
+$body = Get-Content "informationToBeMailed.txt"
+ 
+Send-MailMessage -to "DOMAIN_USERNAME <DOMAIN_USERNAME@DOMAIN.com>" -From "DOMAIN_USERNAME <DOMAIN_USERNAME@DOMAIN.com>" -Subject $subject -Body $body -SmtpServer "SERVER_NAME"
