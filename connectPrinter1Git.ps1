de242c8fc0c7b3f3ad39461081792082bac68483
AddisonDunn <DunnAddisonW@gmail.com>
2016-06-22 10:10:30 -0400

    connectPrinter powershell script
    connect the computer to a printer on a domain

---
+++ connectPrinter1Git.ps1
@@ -0,0 +1,11 @@
+﻿$username = "USERNAME"
+$password ="PASSWORD"
+$secstr = New-Object -TypeName System.Security.SecureString
+$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $secstr
+
+# The connection name can be found at Control Panel -> Printers -> Properties of the desired printer
+$connectionName = "\\PRINTER_CONNECTION\PRINTER_NAME"
+
+# Adds printer based on connection name
+add-printer -connectionname $connectionName
