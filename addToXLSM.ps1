3cd780425d96b4dd7bc112d3db5a6b411e373dfc
AddisonDunn <DunnAddisonW@gmail.com>
2016-06-22 10:06:23 -0400

    addToXLSM powershell script
    Adds the username and computer's name on the domain to the last row of an Excel Sheet

---
+++ addToXLSM.ps1
@@ -0,0 +1,21 @@
+﻿$excel_file_path = 'Q:\New Laptop Tracking\computerNameBook.xlsm' 
+# Instantiate the COM object
+$Excel = New-Object -ComObject Excel.Application
+$Excel.DisplayAlerts = $false
+$ExcelWorkBook = $Excel.Workbooks.Open($excel_file_path)
+# First Sheet in Workbook
+$ExcelWorkSheet = $Excel.Sheets.item(1)
+$ExcelWorkSheet.activate()
+# Gets username and computer name on the domain
+$Name = [Environment]::UserName 
+$ComputerName = $env:Computername
+# Find the first empty row
+$row = $ExcelWorkSheet.UsedRange.Rows.Count + 1
+$ExcelWorkSheet.Cells.Item($row,1) = $Name
+$ExcelWorkSheet.Cells.Item($row,2) = $ComputerName 
+ 
+$ExcelWorkBook.Save()
+$ExcelWorkBook.Close()
+$Excel.Quit()
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
+Stop-Process -Name EXCEL -Force
