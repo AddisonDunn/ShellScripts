4b2a455fe17dffbf80add37018256d37be58c3eb
Addison Dunn <DunnAddisonW@gmail.com>
2016-08-17 15:36:12 -0400

    Add files via upload

---
+++ resetDefaultPDF.ps1
@@ -0,0 +1,12 @@
+﻿for /f “usebackq” %i in (`reg query hkcr /f “AppX”`) do reg query %i\DefaultIcon | find “MicrosoftEdgePDF” && reg add %i /v NoOpenWith /t REG_SZ /f
+
+$key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+(Get-ItemProperty -Path $key -Name ProgramFilesDir).ProgramFilesDir
+
+
+$registryPath = "HKEY_CURRENT_USER\SOFTWARE\Classes\" + $key
+
+$Name = "NoOpenWith"
+
+
+New-ItemProperty -Path $registryPath -Name $name -Value $value `-PropertyType DWORD -Force | Out-Null
